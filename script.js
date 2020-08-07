eb991c831f675cba4977342aa353c8b3fdbd7109
Artem-eduard <artemshykurin98@gmail.com>
2020-08-07 16:25:49 +0900

    updated few

--- script.js
+++ script.js
@@ -8,13 +8,25 @@ function loadVariable()
 		}
 		else
 		{
-			$("#div_var1").show();	
-			$("#div_var2").show();	
 			var datas = data.split(",");
-            $('#div_var1').html("<h2>" + datas[0] + "</h2>");
+			$('#div_var1').html("<h2>" + datas[0] + "</h2>");
 			$('#div_var2').html("<h2>" + datas[1] + "</h2>");
+			
+			if (datas[2] == true)
+			{
+				$("#div_var1").show();	
+				$("#div_var2").show();	
+            
+			}
+			else
+			{
+				$("#div_var1").hide();	
+				$("#div_var2").hide();	
+				
+			}
 						
 		}
+		setTimeout(loadVariable, 10000);
 	}).fail(function(data) {
 		console.log('Error: ' + data);
 		$("#div_var1").hide();	
@@ -27,5 +39,3 @@ $(document).ready(function(){
 	$("#div_var2").hide();	
    loadVariable();
 });
-
-setInterval(loadVariable(), 10000);
--- service.php
+++ service.php
@@ -1,10 +1,11 @@
 <?php
-$var1= 2323.23;
+$var1= 2343.23;
 $var2= 230;
+$show_flag = true;
 
         if($var1 && $var2)
         {
-           echo $var1 . "," . $var2;
+           echo $var1 . "," . $var2 . "," . $show_flag;
    
         }
         else
